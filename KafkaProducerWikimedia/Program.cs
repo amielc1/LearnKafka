4f6bbbe9e8967dcb77e3d8d4b7d744405f41901b
Amiel Cohen <amielc1@gmail.com>
2024-04-09 17:49:10 +0300

    add dependency injection

--- KafkaProducerWikimedia/Program.cs
+++ KafkaProducerWikimedia/Program.cs
@@ -1,16 +1,37 @@
-﻿using WikimediaKafkaProducer.Services;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using WikimediaKafkaProducer.Services;
 
-
-var httpClient = new HttpClient();
-var eventStreamService = new EventStreamService(httpClient);
-var kafkaProducerService = new KafkaProducerService("localhost:19092", "wikimedia-recent-changes");
-
-Console.WriteLine("Starting to listen to Wikimedia recent changes...");
-
-await foreach (var line in eventStreamService.GetEventsAsync())
+namespace WikimediaKafkaProducer
 {
-    Console.WriteLine($"Producing record: {line}");
-    await kafkaProducerService.ProduceAsync(line);
-}
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
 
-kafkaProducerService.Flush();
+            var eventStreamService = host.Services.GetRequiredService<EventStreamService>();
+            var kafkaProducerService = host.Services.GetRequiredService<KafkaProducerService>();
+
+            Console.WriteLine("Starting to listen to Wikimedia recent changes...");
+
+            await foreach (var line in eventStreamService.GetEventsAsync())
+            {
+                Console.WriteLine($"Producing record: {line}");
+                await kafkaProducerService.ProduceAsync(line);
+            }
+
+            kafkaProducerService.Flush();
+        }
+
+        static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((_, services) =>
+                    services.AddSingleton<HttpClient>()
+                        .AddSingleton<EventStreamService>()
+                        .AddSingleton<KafkaProducerService>(sp => new KafkaProducerService("localhost:19092", "wikimedia-recent-changes3")));
+    }
+}
